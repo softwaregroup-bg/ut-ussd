8f1409be8368112671b1d51b0eca40dd06364fbc
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2015-11-20 18:25:32 +0200

    feat(initRoutes): miltiple servers

--- index.js
+++ index.js
@@ -1,96 +1,110 @@
 var when = require('when');
 var Path = require('path');
-var initRoute;
+var registerRequestHandlers;
 var session;
 var ussd;
 var config;
+var registeredHandlersCache = {};
+
 module.exports = {
     init: function(bus) {
-        initRoute = bus.importMethod('internal.registerRequestHandler');
+        registerRequestHandlers = [];
         session = require('./lib/session')({bus: bus});
         ussd = require('./lib/ussd')({bus: bus});
         config = bus.config.ussd;
+        if (config.registerRequestHandlers && config.registerRequestHandlers.length > 0){
+            for (var i = 0, rrhl = config.registerRequestHandlers.length; i < rrhl; i = i + 1) {
+                registerRequestHandlers.push({f: bus.importMethod(config.registerRequestHandlers[i]), name: config.registerRequestHandlers[i]});
+            }
+        }
     },
     initRoutes: function() {
-        initRoute({
-            method: 'GET',
-            path: '/ussd/{p*}',
-            handler: {
-                directory: {
-                    path: Path.join(__dirname, 'public'),
-                    listing: false,
-                    index: true
+        var self = this;
+        registerRequestHandlers.forEach(function(registerRequestHandler) {
+            if (registeredHandlersCache[registerRequestHandler.name]) {//already registered, skipping
+                return;
+            }
+            registeredHandlersCache[registerRequestHandler.name] = 1;
+            registerRequestHandler.f({
+                method: 'GET',
+                path: '/ussd/{p*}',
+                handler: {
+                    directory: {
+                        path: Path.join(__dirname, 'public'),
+                        listing: false,
+                        index: true
+                    }
                 }
-            }
-        });
-        initRoute({
-            method: 'GET',
-            path: '/ussd/session',
-            handler: function(request, reply) {
-                session.get()
-                    .then(function(data) {
-                        var response = '';
-                        if (!data) {
-                            response = 'db empty';
-                        } else {
-                            data.forEach(function(record) {
-                                response += '<div style="border-bottom: 1px dashed grey; padding: 20px">' +
-                                    record.key + ': ' + JSON.stringify(record.value, null, 4) +
-                                    '</div>';
-                            })
-                        }
-                        reply('<span style="white-space: pre; font-family: \'Courier New\', Courier, monospace">' +
-                            response +
-                            '</span>');
-                    })
-                    .done();
-            }
-        });
-        initRoute({
-            method: 'GET',
-            path: '/ussd/session/{key}',
-            handler: function(request, reply) {
-                session.get(request.params.key)
-                    .then(function(value) {
-                        if (!value) {
-                            reply('no session data');
-                        } else {
+            });
+            registerRequestHandler.f({
+                method: 'GET',
+                path: '/ussd/session',
+                handler: function(request, reply) {
+                    session.get()
+                        .then(function(data) {
+                            var response = '';
+                            if (!data) {
+                                response = 'db empty';
+                            } else {
+                                data.forEach(function(record) {
+                                    response += '<div style="border-bottom: 1px dashed grey; padding: 20px">' +
+                                        record.key + ': ' + JSON.stringify(record.value, null, 4) +
+                                        '</div>';
+                                })
+                            }
                             reply('<span style="white-space: pre; font-family: \'Courier New\', Courier, monospace">' +
-                                JSON.stringify(value, null, 4) +
+                                response +
                                 '</span>');
-                        }
-                    })
-                    .done();
-            }
-        });
-        initRoute({
-            method: 'POST',
-            path: '/ussd',
-            handler: function(request, reply) {
-                this.request(request.payload)
-                    .then(function(data) {
-                        reply(ussd.buildResponse(data));
-                    })
-                    .done();
-            }.bind(this)
-        });
-        initRoute({
-            method: 'POST',
-            path: '/closeUSSDSession',
-            handler: function(request, reply) {
-                session.del(request.payload.phone)
-                    .then(function() {
-                        reply(ussd.buildResponse({
-                            shortMessage: 'Session Closed'
-                        }));
-                    })
-                    .catch(function() {
-                        reply(ussd.buildResponse({
-                            shortMessage: 'error occurred when deleting the session data'
-                        }));
-                    })
-                    .done();
-            }
+                        })
+                        .done();
+                }
+            });
+            registerRequestHandler.f({
+                method: 'GET',
+                path: '/ussd/session/{key}',
+                handler: function(request, reply) {
+                    session.get(request.params.key)
+                        .then(function(value) {
+                            if (!value) {
+                                reply('no session data');
+                            } else {
+                                reply('<span style="white-space: pre; font-family: \'Courier New\', Courier, monospace">' +
+                                    JSON.stringify(value, null, 4) +
+                                    '</span>');
+                            }
+                        })
+                        .done();
+                }
+            });
+            registerRequestHandler.f({
+                method: 'POST',
+                path: '/ussd',
+                handler: function(request, reply) {
+                    self.request(request.payload)
+                        .then(function(data) {
+                            reply(ussd.buildResponse(data));
+                        })
+                        .done();
+                }
+            });
+            registerRequestHandler.f({
+                method: 'POST',
+                path: '/closeUSSDSession',
+                handler: function(request, reply) {
+                    session.del(request.payload.phone)
+                        .then(function() {
+                            reply(ussd.buildResponse({
+                                shortMessage: 'Session Closed'
+                            }));
+                        })
+                        .catch(function() {
+                            reply(ussd.buildResponse({
+                                shortMessage: 'error occurred when deleting the session data'
+                            }));
+                        })
+                        .done();
+                }
+            });
         });
     },
     request: function(msg) {
