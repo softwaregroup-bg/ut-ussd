198781c2bbffc3c06be7578d7714f8d1a14f0c41
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-16 17:41:00 +0200

    fix: validations

--- validations.js
+++ validations.js
@@ -1,13 +1,15 @@
 var joi = require('joi');
 module.exports = {
     'request': {
+        auth: false,
         params: joi.object({
             phone: joi.string().label('phone'),
             message: joi.string().label('message')
         }),
         result: joi.object({
             shortMessage: joi.string().label('short message'),
-            sourceAddr: joi.string().label('source address')
+            sourceAddr: joi.string().label('source address'),
+            debug: joi.object().label('debug info')
         })
     }
 };
